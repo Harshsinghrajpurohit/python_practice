0b3e339e41598c58d536a0c71074c0cbd24dc195
Harshsinghrajpurohit <harshrajpurohit412005@gmail.com>
2026-04-09 22:01:02 +0530

    Added prog3

---
+++ prog3.py
@@ -0,0 +1,24 @@
+#61. write A python program to define a function that returns the square of a number.
+'''def square(n):
+    return n**2
+num=int(input("enter the number:"))
+print("the square of the number is:",square(num))'''
+
+
+#62 Write a python program to define a function if a number is prime.
+'''def is_prime(n):
+    if n<=1:
+        print("the number is not prime")
+    for i in range(2,n):
+        if n%i==0:
+            print("the number is not prime")
+            break  
+    else:
+        print("the number is prime")
+   
+num=int(input("enter the number:")) 
+is_prime(num)'''
+
+#63. Write a python program to define a function that calculates the factorial of a number using recursion.
+
+   
