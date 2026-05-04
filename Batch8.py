c5a4b5060cdaca5d3cfaf47f1019b7865b86bd93
Harshsinghrajpurohit <harshrajpurohit412005@gmail.com>
2026-05-04 22:24:35 +0530

    updated practice.py

--- Batch8.py
+++ Batch8.py
@@ -332,11 +332,21 @@ for i in range(n):
             print(' ',end='')
     print()'''
 
-n = 5
+'''n = 5
 for i in range(1, n + 1):
     for j in range(1, 2 * n):
         if i == 1 or j == i or j == 2 * n - i:
             print('*', end=' ')
         else:
             print(' ', end=' ')
+    print()'''
+
+n = int(input("Enter number of rows: "))
+
+num = 1
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
     print()
